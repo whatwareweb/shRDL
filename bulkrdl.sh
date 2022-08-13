dbe59cf414361e9a2c91638790ac2e794f57b942
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-08-12 21:44:13 -0400

    rename to repolist.txt

--- bulkrdl.sh
+++ bulkrdl.sh
@@ -3,8 +3,8 @@ if ! [ -f shrdl.sh ]; then
     printf "shrdl.sh not found.\n"
     printf "make sure shrdl.sh in in the same directory as this script.\n"
     exit 1
-elif ! [ -f repos.txt ]; then
-    printf "repos.txt file not found.\n"
+elif ! [ -f repolist.txt ]; then
+    printf "repolist.txt file not found.\n"
     printf "Create a file in the same directory as this script containing the URLs for repos you want to download"
     exit 1
 fi
@@ -12,6 +12,6 @@ fi
 while read -r line; do
     printf "Downloading repo: %s\n" "$line"
     ./shrdl "$line"
-done < ./repos.txt
+done < ./repolist.txt
 
 printf "Done!\n"
